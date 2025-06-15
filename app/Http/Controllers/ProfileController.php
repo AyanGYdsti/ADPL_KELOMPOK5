2eec677099d230134d2849290c39a62495849b76
WaOdeZahraRamadani0410 <waodezahrar@gmail.com>
2025-06-15 18:19:51 +0800

    update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class ProfilController extends Controller
+class ProfileController extends Controller
 {
     public function index()
     {
